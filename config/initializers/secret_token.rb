91a944b379846da3ef1de61e547aa1e04e9bad4b
aulia <kingmalprax@gmail.com>
2013-05-27 16:31:55 +0800

    pertama

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aulia::Application.config.secret_token = '3b8036fb34324528c9a8d2dab49fdb4e2c92a3b9676ef5ad4974c2dddf6e8d649a5b64933b14430eb4a55078b359037dd9026f3b56eebaa42d7182941730a4fb'
